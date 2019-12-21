c4dfbfc91133820226fbd4377b57a82ad66bbdf4
3735928559 <56690932+3735928559@users.noreply.github.com>
2019-12-21 11:36:32 +0800

    Updated selection

---
+++ Controllers/ChoiceController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TeaHouse.Models;
+
+namespace TeaHouse.Controllers
+{
+    public class ChoiceController : Controller
+    {
+        private OrderContext db = new OrderContext();
+
+        // GET: Choice
+        public ActionResult Index()
+        {
+            return View(db.Choices.ToList());
+        }
+
+        // GET: Choice/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Choice choice = db.Choices.Find(id);
+            if (choice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(choice);
+        }
+
+        // GET: Choice/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Choice/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,User,OrderTime,Status")] Choice choice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Choices.Add(choice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(choice);
+        }
+
+        // GET: Choice/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Choice choice = db.Choices.Find(id);
+            if (choice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(choice);
+        }
+
+        // POST: Choice/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,User,OrderTime,Status")] Choice choice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(choice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(choice);
+        }
+
+        // GET: Choice/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Choice choice = db.Choices.Find(id);
+            if (choice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(choice);
+        }
+
+        // POST: Choice/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Choice choice = db.Choices.Find(id);
+            db.Choices.Remove(choice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
