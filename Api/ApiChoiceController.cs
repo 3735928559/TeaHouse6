198e0471abd5883791e240666d7346da32ae3a40
Alan Chan <20069380@hkuspace.hku.hk>
2019-12-24 15:30:59 +0800

    Created JSON API

---
+++ Api/ApiChoiceController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TeaHouse.Models;
+
+namespace TeaHouse.Api
+{
+    public class ApiChoiceController : ApiController
+    {
+        private OrderContext db = new OrderContext();
+
+        // GET: api/ApiChoice
+        public IQueryable<Choice> GetChoices()
+        {
+            return db.Choices;
+        }
+
+        // GET: api/ApiChoice/5
+        [ResponseType(typeof(Choice))]
+        public IHttpActionResult GetChoice(int id)
+        {
+            Choice choice = db.Choices.Find(id);
+            if (choice == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(choice);
+        }
+
+        // PUT: api/ApiChoice/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutChoice(int id, Choice choice)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != choice.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(choice).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChoiceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ApiChoice
+        [ResponseType(typeof(Choice))]
+        public IHttpActionResult PostChoice(Choice choice)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Choices.Add(choice);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = choice.Id }, choice);
+        }
+
+        // DELETE: api/ApiChoice/5
+        [ResponseType(typeof(Choice))]
+        public IHttpActionResult DeleteChoice(int id)
+        {
+            Choice choice = db.Choices.Find(id);
+            if (choice == null)
+            {
+                return NotFound();
+            }
+
+            db.Choices.Remove(choice);
+            db.SaveChanges();
+
+            return Ok(choice);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ChoiceExists(int id)
+        {
+            return db.Choices.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Api/ApiOrderController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TeaHouse.Models;
+
+namespace TeaHouse.Api
+{
+    public class ApiOrderController : ApiController
+    {
+        private OrderContext db = new OrderContext();
+
+        // GET: api/ApiOrder
+        public IQueryable<OrderModels> GetOrderModels()
+        {
+            return db.OrderModels;
+        }
+
+        // GET: api/ApiOrder/5
+        [ResponseType(typeof(OrderModels))]
+        public IHttpActionResult GetOrderModels(int id)
+        {
+            OrderModels orderModels = db.OrderModels.Find(id);
+            if (orderModels == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(orderModels);
+        }
+
+        // PUT: api/ApiOrder/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOrderModels(int id, OrderModels orderModels)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != orderModels.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(orderModels).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderModelsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ApiOrder
+        [ResponseType(typeof(OrderModels))]
+        public IHttpActionResult PostOrderModels(OrderModels orderModels)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.OrderModels.Add(orderModels);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = orderModels.Id }, orderModels);
+        }
+
+        // DELETE: api/ApiOrder/5
+        [ResponseType(typeof(OrderModels))]
+        public IHttpActionResult DeleteOrderModels(int id)
+        {
+            OrderModels orderModels = db.OrderModels.Find(id);
+            if (orderModels == null)
+            {
+                return NotFound();
+            }
+
+            db.OrderModels.Remove(orderModels);
+            db.SaveChanges();
+
+            return Ok(orderModels);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderModelsExists(int id)
+        {
+            return db.OrderModels.Count(e => e.Id == id) > 0;
+        }
+    }
+}
